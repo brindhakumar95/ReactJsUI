94494cf052560306ecfdb0142b5c894acfc4a4f4
K <626183@cognizant.com>
2019-01-22 17:31:11 +0530

    frist commit

---
+++ src/friends/friendcard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+class FriendCard extends React.Component {
+    state = {  }
+  
+
+    render() { 
+        return (  
+            <div class="col-md-2">
+            <div class="card" style={{ width: `18rem` }}>
+                <img src={`images/${this.props.pic}`} class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                    <h5 class="card-title">{this.props.title}</h5>
+                    <p class="card-text">{this.props.desc}</p>
+
+                    <a href={this.props.link} class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            </div>
+        );
+    }
+}
+ 
+export default FriendCard;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import NavBar from './navbar/navbar';
+import Carousel from './carousel/carousel';
+import FooterComponet from './footer/footer';
+import CardComponent from './card/card';
+import FriendComponent from './friends/friend';
+ReactDOM.render(
+<span><NavBar></NavBar>
+    
+    <Carousel></Carousel>
+  
+   <CardComponent></CardComponent>
+   <FriendComponent></FriendComponent>
+   <FooterComponet></FooterComponet>
+    </span>
+    ,document.getElementById('root'))
